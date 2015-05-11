d4c1b66a3d2c16b250da87b559c17cb1d278f6e0
Matt <matthewsre@hotmail.om>
2015-05-11 12:41:54 -0700

    Created test project to test service

---
+++ OCC.Service.Webhost.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using OCC.Data;
+using OCC.Service.Webhost.Services;
+
+namespace OCC.Service.Webhost.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        public static CodeCampService GetTestService()
+        {
+            var kernel = new StandardKernel();
+            return kernel.Get<CodeCampService>();
+        }
+    }
+}
---
+++ OCC.Service.Webhost.Tests/Session/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OCC.Service.Webhost.Tests.Session
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
