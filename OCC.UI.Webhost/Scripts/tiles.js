6eb77acfeb093bd6b27735ccd04b29c376916bc1
Mike <cmichaelhansen@gmail.com>
2015-06-04 11:43:41 -0700

    Fix for Issue #9, making entire tile clickable Fixed up anchor tag around each tile, also added style and some javascript to keep the underline option when user mouses over tile to show it's clickable Changed Sponsor link so that it always takes user to sponsors page for sponsor list and info for potential sponsors

---
+++ OCC.UI.Webhost/Scripts/tiles.js
@@ -0,0 +1,31 @@
+﻿tiles = (function() {
+
+    $(document).ready(function() {
+        tiles.onDocumentReady();
+    });
+
+    return {
+        onDocumentReady: function() {
+            tiles.wireUpTiles();
+        },
+
+        wireUpTiles: function() {
+
+            $(".tile").mouseover(function() {
+                var element = $(this).find(".labelLowerLeft");
+                if (element != null) {
+                    element.addClass("underline");
+                }
+            });
+
+            $(".tile").mouseout(function() {
+                var element = $(this).find(".labelLowerLeft");
+                if (element != null) {
+                    element.removeClass("underline");
+                }
+            });
+            
+        }
+
+    };
+})()
