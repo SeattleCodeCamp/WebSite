ff8c91b8a8125f96ab19185c85b68f26bab1f8a5
Slobodan Stipic <slobo80@gmail.com>
2017-03-05 06:51:16 -0800

    Scripts for setting up dev machine complete. Readme.md updated with instructions.

--- SetupDevMachine.ps1
+++ SetupDevMachine.ps1
@@ -1,6 +1,12 @@
-﻿cls
+﻿<#
+This script creates a LocalDB instance and a CodeCamp database. It also seeds the database with sample values. 
+There must be a single default Event in the event table in order for app to run.
+If the LocalDB already exists then it does not try to create it. 
+If the CodeCamp database already exist then it does not try to create it or seed it. It is up to you to ensure it contains the necessary values.
+#>
+cls
 
-$localDBInstance = "MSSQLLocalDB231"
+$localDBInstance = "MSSQLLocalDB"
 $dbname = "CodeCamp"
 
 function ExecuteQuery {
@@ -8,7 +14,6 @@ function ExecuteQuery {
     $conn = new-object ('System.Data.SqlClient.SqlConnection')
     $connString = "Server=$server;Integrated Security=SSPI;Database=$database"
     $conn.ConnectionString = $connString
-    Write-Host $conn.ConnectionString
     $conn.Open()
     $sqlCmd = New-Object System.Data.SqlClient.SqlCommand
     $sqlCmd.CommandText = $query
@@ -31,24 +36,44 @@ function QuerySQL {
         For ($i = 0; $i -le $max; $i++) {
             $name = $data.GetName($i)
             $obj | Add-Member Noteproperty $name -value $data.GetValue($i)
-     }
+        }
+
      $obj
     }
 }
 
 function CreateDb {
-    ExecuteQuery $server "create database $dbname" "master" $false
+    $result = ExecuteQuery $server "create database $dbname" "master" $false
+
+    if($result -ne -1){
+        Write-Host "Failed to create database. Exiting..."
+        exit
+    }
 }
 
 function CreateTables {
-#    $path ="CC.Data.Database\dbo\Tables\"
     $path ="CC.Data.Database\"
-#    $files = "EventAttendees.sql","EventAttendeeRatings.sql","EventAttendeeSessionRatings.sql","Tasks.sql","PersonTasks.sql","SessionAttendees.sql","Sponsors.sql"
-    $files = "create.sql"
-    foreach ($file in $files) {
-        Write-Host "Executing $file"
-        $sql = Get-Content "$path$file"
-        ExecuteQuery $server $sql $dbname $false
+    $file = "create.sql"
+    Write-Host "Executing $file"
+    $sql = Get-Content "$path$file"
+    $result = ExecuteQuery $server $sql $dbname $false
+
+    if ($result -ne -1){
+        Write-Host "Failed to create tables without constraints. Exiting..."
+        exit
+    }
+}
+
+function AddConstraints {
+    $path ="CC.Data.Database\"
+    $file = "AddConstraints.sql"
+    Write-Host "Executing $file"
+    $sql = Get-Content "$path$file"
+    $result = ExecuteQuery $server $sql $dbname $false
+
+    if ($result -ne -1){
+        Write-Host "Failed to add constraints. Exiting..."
+        exit
     }
 }
 
@@ -57,29 +82,30 @@ function Seed {
     $file = "seed.sql"
     Write-Host "Executing $file"
     $sql = Get-Content "$path$file"
-    ExecuteQuery $server $sql $dbname $false
+    $result = ExecuteQuery $server $sql $dbname $false
 
-<#    $sql = "Insert Into Events(Name, StartTime, EndTime, Address1, Address2, City, State, Zip, IsDefault, IsSponsorRegistrationOpen, IsSpeakerRegistrationOpen, IsAttendeeRegistrationOpen, IsVolunteerRegistrationOpen) Values ('Seattle Code Camp 2011', '03/21/2011', '03/21/2011', 'Seminole State College', '100 College Dr', 'Sanford', 'FL', '32746', 1, 0, 0, 0, 0)"
-    ExecuteQuery $server $sql $dbname $false
-#>
+    if ($result -lt 1){
+        Write-Host "Failed to seed the database. Exiting..."
+        exit
+    }
 }
 
 function GetLocalInstance {
     $command = "SQLLocalDB i `"$($localDBInstance)`""
     $info = Invoke-Expression $command 
-    $info
+    return $info
 }
 
 function CreateLocalInstance {
     $command = "SQLLocalDB create `"$($localDBInstance)`""
     $info = Invoke-Expression $command
-    $info
+    return $info
 }
 
 function StartLocalInstance {
     $command = "SQLLocalDB start `"$($localDBInstance)`""
     $info = Invoke-Expression $command
-    $info
+    return $info
 }
 
 $instanceInfo = GetLocalInstance
@@ -107,13 +133,19 @@ QuerySQL $server "SELECT * FROM master.dbo.sysdatabases" | ForEach-Object {
   }
 
 if (-not $dbExists) {
-    Write-Host "Creating $dbname"
+    Write-Host "Creating $dbname database"
     CreateDb
+    Write-Host "$dbname database created"
 
     Write-Host "Creating tables that do not need to be seeded."
     CreateTables
+    Write-Host "Tables not needing to be seeded created."
 
     Write-Host "Seeding $dbname with a default sample values and a default Event because it is necessary for site to run."
-#    Seed
+    Seed
+    Write-Host "Seeding compelete."
 
+    Write-Host "Adding constraints to $dbname."
+    AddConstraints
+    Write-Host "Constraints added."
 }
