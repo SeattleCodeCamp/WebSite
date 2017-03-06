1f9360a4e40284063fefe1930fdfc12560986cd4
Slobodan Stipic <slobo80@users.noreply.github.com>
2017-03-05 18:52:27 -0800

    Adding dev setup script (#70)
    * added dev setup script
    
    * dev setup needs to separate create, seed, alter sql scripts
    
    * Scripts for setting up dev machine complete. Readme.md updated with instructions.
    
    * Added .vs folder to gitignore
    
    * revertign changes
    
    * Reverting more unintentional changes.
    
    * Undo

---
+++ CC.Data.Database/SetupScripts/Seed.sql
@@ -0,0 +1,131 @@
+﻿Insert Into People
+(FirstName, LastName, Email, IsAdmin, TShirtSize)
+Values
+('NETDA', 'Admin', 'core@dotnetda.org', 0, 0),
+('John', 'Smith', 'john@dotnetda.org', 0, 0),
+('Brian', 'Hall', 'brian@dotnetda.org', 0, 0),
+('Zdravko', 'Danev', 'z@onetug.org', 0, 0),
+('Esteban', 'Garcia', 'esteban@dotnetda.org', 0, 0),
+('Esteban2', 'Garcia2', 'esteban2@dotnetda.org', 0, 0)
+
+Insert into People
+(Email, FirstName, LastName, Title, Bio, Website, Blog, Twitter, PasswordHash, ImageUrl, IsAdmin, Location, TShirtSize)
+Select
+Email, FirstName, LastName, Title, Bio, Website, Blog, Twitter, PasswordHash, ImageUrl, IsAdmin, Location, TShirtSize
+From People p
+Where p.Email Not In (Select Email From People)
+
+
+Declare @personId Int; Select @personId = (Select Min(Id) From People)
+
+Insert Into Events
+(Name, StartTime, EndTime, Address1, Address2, City, State, Zip, IsDefault, IsSponsorRegistrationOpen, IsSpeakerRegistrationOpen, IsAttendeeRegistrationOpen, IsVolunteerRegistrationOpen)
+Values
+('Seattle Code Camp 2011', '03/21/2011', '03/21/2011', 'Seminole State College', '100 College Dr', 'Sanford', 'FL', '32746', 0, 0, 0, 0, 0),
+('Seattle Code Camp 2012', '03/21/2012', '03/21/2012', 'Seminole State College', '100 College Dr', 'Sanford', 'FL', '32746', 1, 0, 0, 0, 0)
+
+Insert Into Events
+(Name, Description, TwitterHashTag, StartTime, EndTime, Location, Address1, Address2, City, State, Zip, IsDefault, IsSponsorRegistrationOpen, IsSpeakerRegistrationOpen, IsAttendeeRegistrationOpen, IsVolunteerRegistrationOpen)
+Select Name, Description, TwitterHashTag, StartTime, EndTime, Location, Address1, Address2, City, State, Zip, IsDefault, IsSponsorRegistrationOpen, IsSpeakerRegistrationOpen, IsAttendeeRegistrationOpen, IsVolunteerRegistrationOpen
+From Events e
+Where e.Name Not In (Select Name From Events)
+
+Declare @Event1_Id Int; Select @Event1_Id = (Select Id From Events Where name = 'Seattle Code Camp 2011')
+Declare @Event2_Id Int; Select @Event2_Id = (Select Id From Events Where name = 'Seattle Code Camp 2012')
+Insert Into Announcements
+(Event_ID, Title, Content, PublishDate)
+Values 
+(@Event1_Id, 'call for speakers', 'This is the first announcement.', '1/1/2012'),
+(@Event1_Id, 'call for volunteers', 'This is the first announcement.', '2/1/2012'),
+(@Event1_Id, 'call for attendees', 'This is the first announcement.', '3/1/2012'),
+(@Event2_Id, 'call for speakers', 'This is the first announcement.', '1/1/2012'),
+(@Event2_Id, 'call for volunteers', 'This is the first announcement.', '2/1/2012'),
+(@Event2_Id, 'call for attendees', 'This is the first announcement.', '3/1/2012')
+
+Insert Into Announcements
+Select Event_ID, Title, Content, PublishDate
+From Announcements a
+Where Not Exists 
+(Select *
+From Announcements t
+Where t.Title = a.Title and t.PublishDate = a.PublishDate)
+
+Insert Into Timeslots
+(Event_ID, Name, StartTime, EndTime)
+Values  (@Event2_Id, 'Morning Session 1' ,'3/1/2012 9:00:00.000', '3/1/2012 9:50:00.000'),
+		(@Event2_Id, 'Morning Session 2' ,'3/1/2012 10:00:00.000', '3/1/2012 10:50:00.000'),
+		(@Event2_Id, 'Morning Session 3' ,'3/1/2012 11:00:00.000', '3/1/2012 11:50:00.000'),
+		(@Event2_Id, 'Afternoon Session 1' ,'3/1/2012 13:00:00.000', '3/1/2012 13:50:00.000'),
+		(@Event2_Id, 'Afternoon Session 2' ,'3/1/2012 14:00:00.000', '3/1/2012 14:50:00.000'),
+		(@Event2_Id, 'Afternoon Session 3' ,'3/1/2012 15:00:00.000', '3/1/2012 15:50:00.000'),
+		(@Event2_Id, 'Afternoon Session 4' ,'3/1/2012 16:00:00.000', '3/1/2012 16:50:00.000')
+
+Insert Into Timeslots
+(Event_ID, Name, StartTime, EndTime)
+Select Event_ID, Name, StartTime, EndTime
+From Timeslots
+Where Name not in (Select Name From Timeslots)
+
+Declare @TimeslotId Int; Select @TimeslotId = (Select Min(ID) From Timeslots)
+
+Insert Into Tracks
+(Event_ID, Name, Description)
+Values
+(@Event2_Id, '"Windows Phone 7', 'Windows Phone 7 development'),
+(@Event2_Id, 'Windows 8', 'Windows 8 development'),
+(@Event2_Id, 'Architecture', 'Architecture, P and P')
+
+Insert Into Tracks
+Select Event_ID, Name, Description
+From Tracks
+Where Name Not In (Select Name From Tracks)
+
+Insert Into Sessions
+(Event_ID, Speaker_ID, Track_ID, Timeslot_ID, Name, Description, Status, Level)
+Values
+(@Event2_Id, @personId, 1, 1, 'Silverlight for WP7', 'Introduction in Silverlight programming with windows phone 7', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'XNA for WP7', 'Introduction in XNA programming with windows phone 7', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'Intro in Win 8', 'Introduction in Windows 8', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'P & P', 'Patterns and practices', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'Regular Expressions Boot Camp', 'Regular Expressions Boot Camp', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'Running ASP.NET without IIS', 'Running ASP.NET without IIS', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'Git in the enterprise environment', 'Git in the enterprise environment', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'Refactoring Workshop', 'Refactoring Workshop', 'Approved', 100),
+(@Event2_Id, @personId, 1, 1, 'Mocking without Fear', 'Mocking without Fear', 'Approved', 100)
+
+Insert Into Sessions
+(Event_ID, Speaker_ID, Track_ID, Timeslot_ID, Name, Description, Status, Level, Tag_ID)
+Select Event_ID, Speaker_ID, (Select Min(Id) From Tracks), @TimeslotId, Name, Description, Status, Level, null
+From Sessions
+Where Name Not In (Select Name From Sessions)
+
+Insert Into Tags
+(TagName)
+Values 
+('Architecture'),
+('Career'),
+('Cloud'),
+('Data'),
+('Game/VR'),
+('Mobile'),
+('Testing'),
+('User Experience'),
+('Web'),
+('Other'),
+('IoT'),
+('Hardware')
+
+Insert Into Tags
+(TagName)
+Select TagName
+From Tags
+Where TagName Not In (Select TagName From Tags)
+
+Insert Into KeyValuePairs
+Values
+('tshirtSizes', '[{"Item1":1,"Item2":"Don''t want one"},{"Item1":2,"Item2":"Small"},{"Item1":2,"Item2":"Medium"},{"Item1":2,"Item2":"Large"},{"Item1":2,"Item2":"X-Large"},{"Item1":2,"Item2":"XX-Large"}]')
+
+Insert Into KeyValuePairs
+Select *
+From KeyValuePairs
+Where Id Not In (Select Id From KeyValuePairs)
---
+++ SetupDevMachine.ps1
@@ -0,0 +1,151 @@
+﻿<#
+This script creates a LocalDB instance and a CodeCamp database. It also seeds the database with sample values. 
+There must be a single default Event in the event table in order for app to run.
+If the LocalDB already exists then it does not try to create it. 
+If the CodeCamp database already exist then it does not try to create it or seed it. It is up to you to ensure it contains the necessary values.
+#>
+cls
+
+$localDBInstance = "MSSQLLocalDB"
+$dbname = "CodeCamp"
+
+function ExecuteQuery {
+    Param ($server, $query, $database, $read = $true)
+    $conn = new-object ('System.Data.SqlClient.SqlConnection')
+    $connString = "Server=$server;Integrated Security=SSPI;Database=$database"
+    $conn.ConnectionString = $connString
+    $conn.Open()
+    $sqlCmd = New-Object System.Data.SqlClient.SqlCommand
+    $sqlCmd.CommandText = $query
+    $sqlCmd.Connection = $conn
+    if ($read) {
+        $Rset = $sqlCmd.ExecuteReader()
+    }
+    else {
+        $Rset = $sqlCmd.ExecuteNonQuery()
+    }
+    ,$Rset ## The comma is used to create an outer array, which PS strips off automatically when returning the $Rset
+}
+
+function QuerySQL {
+    Param ($server, $query, $database = "master")
+    $data = ExecuteQuery $server $query $database
+    while ($data.read() -eq $true) {
+        $max = $data.FieldCount -1
+        $obj = New-Object Object
+        For ($i = 0; $i -le $max; $i++) {
+            $name = $data.GetName($i)
+            $obj | Add-Member Noteproperty $name -value $data.GetValue($i)
+        }
+
+     $obj
+    }
+}
+
+function CreateDb {
+    $result = ExecuteQuery $server "create database $dbname" "master" $false
+
+    if($result -ne -1){
+        Write-Host "Failed to create database. Exiting..."
+        exit
+    }
+}
+
+function CreateTables {
+    $path ="CC.Data.Database\"
+    $file = "create.sql"
+    Write-Host "Executing $file"
+    $sql = Get-Content "$path$file"
+    $result = ExecuteQuery $server $sql $dbname $false
+
+    if ($result -ne -1){
+        Write-Host "Failed to create tables without constraints. Exiting..."
+        exit
+    }
+}
+
+function AddConstraints {
+    $path ="CC.Data.Database\"
+    $file = "AddConstraints.sql"
+    Write-Host "Executing $file"
+    $sql = Get-Content "$path$file"
+    $result = ExecuteQuery $server $sql $dbname $false
+
+    if ($result -ne -1){
+        Write-Host "Failed to add constraints. Exiting..."
+        exit
+    }
+}
+
+function Seed {
+    $path ="CC.Data.Database\"
+    $file = "seed.sql"
+    Write-Host "Executing $file"
+    $sql = Get-Content "$path$file"
+    $result = ExecuteQuery $server $sql $dbname $false
+
+    if ($result -lt 1){
+        Write-Host "Failed to seed the database. Exiting..."
+        exit
+    }
+}
+
+function GetLocalInstance {
+    $command = "SQLLocalDB i `"$($localDBInstance)`""
+    $info = Invoke-Expression $command 
+    return $info
+}
+
+function CreateLocalInstance {
+    $command = "SQLLocalDB create `"$($localDBInstance)`""
+    $info = Invoke-Expression $command
+    return $info
+}
+
+function StartLocalInstance {
+    $command = "SQLLocalDB start `"$($localDBInstance)`""
+    $info = Invoke-Expression $command
+    return $info
+}
+
+$instanceInfo = GetLocalInstance
+
+if($instanceInfo.Get(0) -like "*failed*"){
+    Write-Host "Could not find $localDBInstance."
+    Write-Host "Creating $localDBInstance"
+
+    CreateLocalInstance
+}
+
+StartLocalInstance
+
+$pair = GetLocalInstance | where {$_ -like '*pipe*'} 
+$len = $pair.Length
+$server = $pair.Substring($pair.IndexOf('np:\\.\pipe\LOCALDB'), $len - $pair.IndexOf('np:\\.\pipe\LOCALDB'))
+$dbExists = $false
+
+QuerySQL $server "SELECT * FROM master.dbo.sysdatabases" | ForEach-Object {
+  if ($_.name -eq $dbname){
+        Write-Host "Found $dbname database."
+        $dbExists = $true
+        break;
+     }
+  }
+
+if (-not $dbExists) {
+    Write-Host "Creating $dbname database"
+    CreateDb
+    Write-Host "$dbname database created"
+
+    Write-Host "Creating tables that do not need to be seeded."
+    CreateTables
+    Write-Host "Tables not needing to be seeded created."
+
+    Write-Host "Seeding $dbname with a default sample values and a default Event because it is necessary for site to run."
+    Seed
+    Write-Host "Seeding compelete."
+
+    Write-Host "Adding constraints to $dbname."
+    AddConstraints
+    Write-Host "Constraints added."
+}
