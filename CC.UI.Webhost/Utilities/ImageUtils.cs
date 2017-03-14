a8aaa123884ad4d19eb0da3e79b8d78db6ec813a
Tige Williams <tigewilliams@users.noreply.github.com>
2017-03-14 09:57:25 -0700

    Fixes issue #41 - Editing a sponsor required image upload. (#73)
    * Added ImageUtils class to take care of dealing with nulls and corrupted image data.
    
    * Updated CodeCampService to not update the sponsor image if one wasn't supplied.  This prevents the image from being accidentally deleted.
    
    * Updated some controllers to use the ImageUtils.ImageFromBytes helper function.
    
    * Moved some Sponsor mapping code into helper functions so that we reuse it.  Also, converted a loop to be a single LINQ Select statement using the helper function.
    
    * Moved the code that creates Metro tiles for the Sponsors into a private helper function.

---
+++ CC.UI.Webhost/Utilities/ImageUtils.cs
@@ -0,0 +1,38 @@
+﻿using CC.UI.Webhost.Infrastructure;
+
+namespace OCC.UI.Webhost.Utilities
+{
+    /// <summary>
+    /// Helper functions to deal with images.
+    /// </summary>
+    public static class ImageUtils
+    {
+        /// <summary>
+        /// Creates a WebImageOCC from a Byte Array.
+        /// </summary>
+        /// <param name="imageBytes">Byte array containing the image data.</param>
+        /// <returns>WebImageOCC created from the image data, or null if no data was present.</returns>
+        /// <exception cref=""
+        public static WebImageOCC ImageFromBytes(byte[] imageBytes)
+        {
+            // If there isn't any data, just return null
+            if (imageBytes == null || imageBytes.Length == 0)
+            {
+                return null;
+            }
+
+            try
+            {
+                return new WebImageOCC(imageBytes);
+            }
+            catch
+            {
+                // Just eat the exception and return Null for now.
+                // It would probably be better to throw some kind of "corrupt image data"
+                // exception and then setup logging to notify us of that, but for now this is
+                // good enough I think.
+                return null;
+            }
+        }
+    }
+}
